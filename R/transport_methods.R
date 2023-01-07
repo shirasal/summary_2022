c1b06d562abd8a1cd16463affcfa64f07be97260
Shira Salingre <salingre@mail.tau.ac.il>
2023-01-07 12:37:39 +0100

    improved transportation methods graph

--- R/transport_methods.R
+++ R/transport_methods.R
@@ -6,11 +6,13 @@ transport <- read_csv("data/transport_2022.csv")
   pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
   mutate(month = factor(month, levels = month.name)) %>% 
   filter(method != "Fly") %>% 
-  ggplot() + aes(x = month, y = km, fill = method) + 
-  geom_bar(stat = "identity", position = "dodge") + 
-  ggthemes::scale_fill_wsj(palette = "rgby") +
-  facet_wrap(~method, scale = "free_y", ncol = 1) + 
-  labs(x = "Month", y = "Distance [KM]", fill = "Method") +
-  theme(legend.position = "top"))
+  ggplot() + aes(x = month, y = km, fill = method)
+  + geom_bar(stat = "identity", position = "dodge")
+  + ggthemes::scale_fill_wsj(palette = "rgby")
+  + geom_vline(aes(xintercept = 6), linetype = "dotted", col = "gray", lwd = 1.5)
+  + facet_wrap(~method, scale = "free_y", ncol = 1)
+  + labs(x = "", y = "Distance [KM]", fill = "Method")
+  + ggthemes::theme_hc() + theme(legend.position = "none",
+                                    strip.text = element_text(face = "bold")))
 
 ggsave(plot = tran_plot, filename = "plots/transport_2022.png", device = "png")
