074bead988046c7923a148f576d76ba687eb1a0d
Shira Salingre <salingre@mail.tau.ac.il>
2023-01-08 11:13:20 +0100

    combined plots

--- R/transport_methods.R
+++ R/transport_methods.R
@@ -1,49 +1,46 @@
 library(tidyverse)
+library(patchwork)
 
 transport <- read_csv("data/transport_2022.csv") 
 
-transport
+tran_plot <- transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = km, fill = method) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  ggthemes::scale_fill_wsj(palette = "rgby") + 
+  geom_vline(aes(xintercept = 6), linetype = "dotted", col = "gray", lwd = 1.5) + 
+  facet_wrap(~method, scale = "free_y", ncol = 1) +
+  labs(x = "", y = "Distance [KM]", fill = "Method") + 
+  ggthemes::theme_hc() + theme(legend.position = "none",
+                               strip.text = element_text(face = "bold"))
 
-(
-  tran_plot <- transport %>% 
-    pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
-    mutate(month = factor(month, levels = month.name)) %>% 
-    mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>% 
-    filter(method != "Fly") %>% 
-    ggplot() + aes(x = month_lab, y = km, fill = method)
-  + geom_bar(stat = "identity", position = "dodge")
-  + ggthemes::scale_fill_wsj(palette = "rgby")
-  + geom_vline(aes(xintercept = 6), linetype = "dotted", col = "gray", lwd = 1.5)
-  + facet_wrap(~method, scale = "free_y", ncol = 1)
-  + labs(x = "", y = "Distance [KM]", fill = "Method",
-         title = "How far did I travel in 2022",
-         subtitle = "by method",
-         caption = "During the month of June I moved from Israel to Barcelona (marked by a gray dotted line).")
-  + ggthemes::theme_hc() + theme(legend.position = "none",
-                                 strip.text = element_text(face = "bold"))
-)
-
-ggsave(plot = tran_plot, filename = "plots/transport_2022.png", device = "png")
+line_trans <- transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>%
+  group_by(month) %>% 
+  mutate(relative_use = (km/sum(km))*100) %>% 
+  ungroup() %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = relative_use, col = method, group = method) +
+  geom_point(shape = 18, size = 4) +
+  geom_path(lwd = 2) +
+  geom_vline(aes(xintercept = 6), alpha = .5, col = "gray", lwd = 1.5) +
+  ggthemes::scale_colour_wsj(palette = "rgby") +
+  labs(x = "", y = "Relative distance", col = "Method") + 
+  ggthemes::theme_hc()
 
 
-(
-  line_trans <- transport %>% 
-    pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
-    mutate(month = factor(month, levels = month.name)) %>% 
-    mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>%
-    group_by(month) %>% 
-    mutate(relative_use = (km/sum(km))*100) %>% 
-    ungroup() %>% 
-    filter(method != "Fly") %>% 
-    ggplot() + aes(x = month_lab, y = relative_use, col = method, group = method) +
-    geom_point(shape = 18, size = 4) +
-    geom_path(lwd = 2) +
-    geom_vline(aes(xintercept = 6), alpha = .5, col = "gray", lwd = 1.5) +
-    ggthemes::scale_colour_wsj(palette = "rgby") +
-    labs(x = "", y = "Relative distance", col = "Method",
-         title = "Travel method by relative distance",
-         caption = "During the month of June I moved from Israel to Barcelona (marked by a gray line).") + 
-    ggthemes::theme_hc()
-)
+tran_plot_combined <- tran_plot + line_trans +
+  plot_layout(widths = c(1, 1.3)) + 
+  plot_annotation(
+    title = 'Travel methods in 2022',
+    caption = 'During the month of June I moved from Israel to Barcelona (marked by a gray line).'
+  ) + theme(plot.title = element_text(face = "bold"))
 
-ggsave(plot = line_trans, filename = "plots/transport_2022_line.png", device = "png")
+tran_plot_combined
+
+ggsave(plot = tran_plot_combined, filename = "plots/transport_2022.png", device = "png", scale = c(1.7,1))
--- README.md
+++ README.md
@@ -5,8 +5,7 @@ This repository was inspired by [Storytelling with Data](https://www.storytellin
 2022 was a big year for me.  
 I moved from the country side in Israel to the city of Barcelona (Catalonia, Spain). While still getting used to everything about living in the city, learning the language(s), meeting new people and finding my place, one thing that occurred immediately is the change in my movement behaviour. Barcelona has one of the best public transport systems in the world, and is extremely walkable. In Israel, like many Israelis, I HAD to have a car and this became my main method of transportation. So the move to Barcelona made me walk a lot more again, and use public transport rather than drive a car. This change on its own is a huge life style improvement for me! I am now a proud non-owner of a car :tada:.
 
-![bar plot of transport method distances in 2022](https://github.com/shirasal/summary_2022/blob/main/plots/transport_2022.png)
-![line plot of transport method distances in 2022](https://github.com/shirasal/summary_2022/blob/main/plots/transport_2022_line.png)
+![plots of transport method distances in 2022](https://github.com/shirasal/summary_2022/blob/main/plots/transport_2022.png)
 
 The remarkable change from almost exclusive car use to public transport and walking is well-seen in the plot above.  
 In August we[^*] had some car-travel with friends in Costa Brava. In September and October my brother visited us and we traveled with him outside of the city a bit. We also just moved to a new place around this time and used public transport a lot for shopping for the new place.  
