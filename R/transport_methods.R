c3ce0183ebdff64ea7b9f4119c748684e29e3795
Shira Salingre <salingre@mail.tau.ac.il>
2023-01-24 15:17:13 +0100

    saved separate plots and created more "storytelling" plot of the two combined

--- R/transport_methods.R
+++ R/transport_methods.R
@@ -44,3 +44,109 @@ tran_plot_combined <- tran_plot + line_trans +
 tran_plot_combined
 
 ggsave(plot = tran_plot_combined, filename = "plots/transport_2022.png", device = "png", scale = c(1.7,1))
+ggsave(plot = tran_plot, filename = "plots/transport_bars.png", device = "png")
+ggsave(plot = line_trans, filename = "plots/transport_line.png", device = "png")
+
+
+# More storytelling -------------------------------------------------------
+
+## Bar plots more self-explanatory ----------------------------------------
+
+# Option 1
+transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = km, fill = method) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  ggthemes::scale_fill_wsj(palette = "rgby") + 
+  facet_wrap(~method, scale = "free_y", ncol = 1) +
+  labs(x = "", y = "Distance [KM]", fill = "Method") + 
+  theme(legend.position = "bottom", legend.title = element_text(face = "bold"), 
+        legend.background = element_rect(colour = 'black', fill = 'white', linetype='solid'),
+        strip.background = element_blank(),
+        strip.text.x = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_blank())
+
+# Option 2
+transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = km, fill = method) +
+  geom_bar(stat = "identity", position = "stack") + 
+  ggthemes::scale_fill_wsj(palette = "rgby") + 
+  labs(x = "", y = "Distance [KM]", fill = "Method") + 
+  theme(legend.position = "bottom", legend.title = element_text(face = "bold"), 
+        legend.background = element_rect(colour = 'black', fill = 'white', linetype='solid'),
+        panel.grid = element_blank(),
+        panel.background = element_blank())
+
+
+## Line plot more self-explanatory ----------------------------------------
+
+transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>%
+  group_by(month) %>% 
+  mutate(relative_use = (km/sum(km))) %>% 
+  ungroup() %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = relative_use, col = method, group = method) +
+  geom_point(shape = 18, size = 4) +
+  geom_path(lwd = 2) +
+  ggthemes::scale_colour_wsj(palette = "rgby") +
+  scale_y_continuous(labels = scales::percent) + 
+  labs(x = "", y = "Relative distance", col = "Method") + 
+  theme(legend.position = "bottom", legend.title = element_text(face = "bold"), 
+        legend.background = element_rect(colour = 'black', fill = 'white', linetype='solid'),
+        panel.grid = element_blank(),
+        panel.background = element_rect(colour = 'black', fill = 'white', linetype='solid'))
+
+
+## Two plots combined -----------------------------------------------------
+
+new_tran_bar <- transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = km, fill = method) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  ggthemes::scale_fill_wsj(palette = "rgby") + 
+  facet_wrap(~method, scale = "free_y", ncol = 1) +
+  labs(x = "", y = "Distance [KM]", fill = "Method") + 
+  theme(strip.background = element_blank(),
+        strip.text.x = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_blank(),
+        legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(face = "bold"))
+
+new_tran_line <- transport %>% 
+  pivot_longer(cols = January:December, names_to = "month", values_to = "km") %>% 
+  mutate(month = factor(month, levels = month.name)) %>% 
+  mutate(month_lab = factor(str_sub(month, 1, 3), levels = month.abb)) %>%
+  group_by(month) %>% 
+  mutate(relative_use = (km/sum(km))) %>% 
+  ungroup() %>% 
+  filter(method != "Fly") %>% 
+  ggplot() + aes(x = month_lab, y = relative_use, col = method, group = method) +
+  geom_point(shape = 18, size = 4) +
+  geom_path(lwd = 2) +
+  ggthemes::scale_colour_wsj(palette = "rgby") +
+  scale_y_continuous(labels = scales::percent) + 
+  labs(x = "", y = "Relative distance", col = "Method") + 
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        panel.background = element_rect(colour = 'black', fill = 'white', linetype='solid'))
+
+new_plots <- ggpubr::ggarrange(new_tran_bar, new_tran_line, ncol = 2, common.legend = TRUE, legend = "bottom") + 
+  theme(legend.title = element_blank())
+
+new_plots
+ggsave(plot = new_plots, filename = "plots/new_transport_2022.png", device = "png", scale = c(1.7,1))
+
